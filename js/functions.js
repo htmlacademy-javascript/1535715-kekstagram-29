17d1db990f5561d8b31b1ca248d80538fae0ccab
Gleb <125997986+Al1ve-Outs1de@users.noreply.github.com>
2023-06-22 15:06:41 +0300

    Функции возвращаются (#5)
    * added new function
    
    * fixed fun problems

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,37 @@
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
+export const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-const checkPalindrome = (string) =>{
-  string = string.replaceAll(' ', '').toLowerCase();
-  let reversedString = '';
-  for(let i = string.length; i >= 0; i--){
-    reversedString += string[i];
-  }
-  return string === reversedString;
-}
+export const checkPalindrome = (string) => {
+	string = string.replaceAll(' ', '').toLowerCase();
+	let reversedString = '';
+	for (let i = string.length; i >= 0; i--) {
+		reversedString += string[i];
+	}
+	return string === reversedString;
+};
 
-const makePositiveInteger = (string) =>{
-  string = string.toString();
-  let positiveInteger = '';
-  for(let i = 0; i < string.length; i++){
-    if(!isNaN(parseInt(string[i], 10))){
-      positiveInteger += string[i];
-    }
-  }
-  return positiveInteger === '' ? NaN : Math.abs(parseInt(positiveInteger, 10));
-}
+export const makePositiveInteger = (string) => {
+	string = string.toString();
+	let positiveInteger = '';
+	for (let i = 0; i < string.length; i++) {
+		if (!isNaN(parseInt(string[i], 10))) {
+			positiveInteger += string[i];
+		}
+	}
+	return positiveInteger === '' ? NaN : Math.abs(parseInt(positiveInteger, 10));
+};
+
+const sliceNumbers = (numbers) => numbers.split(':').map(Number);
+
+export const checkMeetingTime = (workStartTime, workEndTime, meetingStartTime, meetingDuration) => {
+	const [workStartHour, workStartMinute] = sliceNumbers(workStartTime);
+	const [workEndHour, workEndMinute] = sliceNumbers(workEndTime);
+	const [meetingStartHour, meetingStartMinute] = sliceNumbers(meetingStartTime);
+	const meetingToMinutes = meetingStartHour * 60 + meetingStartMinute + meetingDuration;
+	const meetingEndHour = Math.floor((meetingToMinutes) / 60);
+	const meetingEndMinute = (meetingToMinutes) % 60;
+
+	const isFinishAfterWork = meetingEndHour > workEndHour || (meetingEndHour === workEndHour && meetingEndMinute > workEndMinute);
+	const isStartBeforeWork = meetingStartHour < workStartHour || (meetingStartHour === workStartHour && meetingStartMinute < workStartMinute);
+
+	return !(isFinishAfterWork || isStartBeforeWork);
+};
