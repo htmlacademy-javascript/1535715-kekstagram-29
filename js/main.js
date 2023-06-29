1235fd0e214d456587dcbeb7f01ff1fe6e22889d
Gleb <gleborlov20022002@gmail.com>
2023-06-29 04:30:38 +0300

    added module to open photo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 import './draw.js';
+import './open-photo.js';
---
+++ js/open-photo.js
@@ -0,0 +1,59 @@
+import { photos } from './mock.js';
+import { isEscapeKey, findElement } from './util.js';
+
+const picturesBlock = document.querySelector('.pictures');
+const bigPicture = document.querySelector('.big-picture');
+const closeButton = document.querySelector('.big-picture__cancel');
+const commentsBlock = document.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+
+const closePhoto = () => {
+	bigPicture.classList.add('hidden');
+	document.body.classList.remove('modal-open');
+	document.querySelector('.social__comments').textContent = '';
+};
+
+const buttonClickHandler = () => {
+	closePhoto();
+};
+const buttonKeydownHandler = (evt) => {
+	if (isEscapeKey(evt)) {
+		closePhoto();
+	}
+};
+
+const showComments = (comments) => {
+	const commentFragment = document.createDocumentFragment();
+
+	comments.forEach((comment) => {
+		const commentElement = commentTemplate.cloneNode(true);
+		const commentAvatar = commentElement.querySelector('.social__picture');
+		commentAvatar.src = comment.avatar;
+		commentAvatar.alt = comment.name;
+		commentElement.querySelector('.social__text').textContent = comment.message;
+		commentFragment.append(commentElement);
+	});
+	commentsBlock.append(commentFragment);
+};
+
+const showFullPhoto = (evt) => {
+	if (evt.target.tagName === 'IMG') {
+		bigPicture.classList.remove('hidden');
+		const chosenPhoto = photos[evt.target.parentNode.dataset.id - 1];
+
+		findElement('.big-picture__img img').src = chosenPhoto.url;
+		findElement('.likes-count').textContent = chosenPhoto.likes;
+		findElement('.comments-count').textContent = chosenPhoto.comments.length;
+		findElement('.social__caption').textContent = chosenPhoto.description;
+		findElement('.social__comment-count').classList.add('hidden');
+		findElement('.comments-loader').classList.add('hidden');
+
+		showComments(chosenPhoto.comments);
+
+		document.body.classList.add('modal-open');
+		closeButton.addEventListener('click', () => buttonClickHandler(), {once: true});
+		document.addEventListener('keydown', (evt) => buttonKeydownHandler(evt), {once: true});
+	}
+};
+
+picturesBlock.addEventListener('click', showFullPhoto);
